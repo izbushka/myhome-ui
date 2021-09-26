510133763361edd23578a255ed128b7a5afd93aa
Oleg Muraviov <omuraviov@datamola.com>
2021-09-26 09:13:43 +0300

    User avatar

--- src/app/shared/components/left-menu/left-menu.container.ts
+++ src/app/shared/components/left-menu/left-menu.container.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {AppState} from '@store/rootReducer';
 import {Observable} from 'rxjs';
@@ -7,23 +7,33 @@ import {SensorsSelectors} from '@store/sensors/selectors';
 import {RouterActions} from '@store/router/actions';
 import {LeftPanelModes} from '@entities/common.interfaces';
 import {CommonActions} from '@store/common/actions';
+import {AuthUser} from '@shared/entities/auth.interfaces';
+import {AuthSelectors} from '@store/auth/selectors';
+import {AuthActions} from '@store/auth/actions';
 
 @Component({
 	selector: 'rpi-left-menu',
 	template: `
 		<rpi-left-menu-component
 			[groups]="sensorGroups$ | async"
+			[user]="user$ | async"
 			(go)="go($event)"
 			(setMode)="setMode($event)"
 		></rpi-left-menu-component>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LeftMenuContainer {
+export class LeftMenuContainer implements OnInit {
 	sensorGroups$: Observable<SensorGroup[]>;
+	user$: Observable<AuthUser>;
 
 	constructor(private store: Store<AppState>) {
 		this.sensorGroups$ = this.store.select(SensorsSelectors.sensorGroups.list);
+		this.user$ = this.store.select(AuthSelectors.user);
+	}
+
+	public ngOnInit(): void {
+		this.store.dispatch(AuthActions.getUser.requested());
 	}
 
 	public go(page: string | string[]): void {
