8ade629e4337aadd4643bbeafdbe35e14e56c317
Oleg Muraviov <omuraviov@datamola.com>
2021-09-27 09:05:59 +0300

    Increase token expiration

--- src/app/shared/entities/common.constants.ts
+++ src/app/shared/entities/common.constants.ts
@@ -1,3 +1,4 @@
 export const SENSORS_POLLING_INTERVAL = 2000;
-export const AUTH_EXPIRATION_TIME = 3600;
+export const AUTH_EXPIRATION_TIME = 86400;
 export const ROUTING_USE_HASH = true;
+export const ADMIN_USER_IDS = [1];
